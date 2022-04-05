ce0c725700f146f4e2b7cce06a82012b02baa47e
Magnus <magn902m@stud.kea.dk>
2022-04-05 16:25:11 +0200

    improved restdb js

--- js/restdb.js
+++ js/restdb.js
@@ -14,46 +14,27 @@ export async function get(endpoint, apikey) {
 
 // GET API
 
-export async function getCarbonApi(carbonApi) {
-  const respone = await fetch(carbonApi);
-  const data = await respone.json();
+export async function getCarbonApi(url) {
+  console.log(url);
+  const carbonApi = "https://kea-alt-del.dk/websitecarbon/site/?url=";
+
+  const response = await fetch(carbonApi + url);
+  const data = await response.json();
 
   return data;
 }
 
-// BUILD
-
-export function build(dataArr) {
-  document.querySelector("#data_input_section .section_wrapper").innerHTML = "";
-
-  dataArr.forEach((item) => {
-    const clone = document.querySelector("template#api_data").content.cloneNode(true);
-
-    // set clone data
-    clone.querySelector("[data-field=company]").textContent = item.company;
-    clone.querySelector("[data-field=url]").textContent = item.url;
-    // clone.querySelector("[data-field=url]").setAttribute("herf", item.url);
-    clone.querySelector("[data-field=industry]").textContent = `${item.industry}`;
-    clone.querySelector("[data-field=green]").textContent = `${item.green}`;
-    clone.querySelector("[data-field=cleanerThen]").textContent = `${item.cleanerThen}%`;
-    clone.querySelector("[data-field=grams_co2]").textContent = `${item.grams_co2} grams`;
-
-    // append clone to list
-    document.querySelector("#data_input_section .section_wrapper").appendChild(clone);
-  });
-}
-
 // POST
 
-export async function post(endpoint, apikey) {
-  const dataObj = {
-    company: "Company",
-    industry: "Industry",
-    url: "url",
-    green: false,
-    cleanerThen: 0.5,
-    grams_co2: 0.8,
-  };
+export async function post(endpoint, apikey, dataObj) {
+  // const dataObj = {
+  //   company: "Company",
+  //   industry: "Industry",
+  //   url: "url",
+  //   green: false,
+  //   cleanerThen: 0.5,
+  //   grams_co2: 0.8,
+  // };
 
   const respone = await fetch(endpoint, {
     method: "post",
