8ad246756cc0205f9a097bfb80dbda6219395b99
Magnus <magn902m@stud.kea.dk>
2022-04-06 14:57:38 +0200

    cleaned up restdb.js

--- js/restdb.js
+++ js/restdb.js
@@ -12,29 +12,11 @@ export async function get(endpoint, apikey) {
   return data;
 }
 
-// GET API
-
-export async function getCarbonApi(url) {
-  console.log(url);
-  const carbonApi = "https://kea-alt-del.dk/websitecarbon/site/?url=";
-
-  const response = await fetch(carbonApi + url);
-  const data = await response.json();
-
-  return data;
-}
-
 // POST
 
-export async function post(endpoint, apikey, dataObj) {
-  // const dataObj = {
-  //   company: "Company",
-  //   industry: "Industry",
-  //   url: "url",
-  //   green: false,
-  //   cleanerThen: 0.5,
-  //   grams_co2: 0.8,
-  // };
+export async function post(endpoint, apikey, payload) {
+  //Object
+  console.log("payload", payload);
 
   const respone = await fetch(endpoint, {
     method: "post",
@@ -42,19 +24,18 @@ export async function post(endpoint, apikey, dataObj) {
       "Content-Type": "application/json",
       "x-apikey": apikey,
     },
-    body: JSON.stringify(dataObj),
+    body: JSON.stringify(payload),
   });
 
   const data = await respone.json();
   console.log(data);
 
-  return data;
+  // return data;
 }
 
 // DELETE
 
-export async function deleteIt(endpoint, apikey) {
-  const id = "6248adb009b5f71a001b8631";
+export async function deleteIt(endpoint, apikey, id) {
   const respone = await fetch(`${endpoint}/${id}`, {
     method: "delete",
     headers: {
@@ -70,17 +51,15 @@ export async function deleteIt(endpoint, apikey) {
 
 // PUT
 
-export async function put(endpoint, apikey) {
-  const dataObj = {
-    company: "Cool company",
-    industry: "Food",
-    url: "www.cool-food.com",
-    green: false,
-    cleanerThen: 0.6,
-    grams_co2: 0.3,
-  };
-
-  const id = "6248ad9a09b5f71a001b8630";
+export async function put(endpoint, apikey, id) {
+  // const dataObj = {
+  //   company: "Cool company",
+  //   industry: "Food",
+  //   url: "www.cool-food.com",
+  //   green: false,
+  //   cleanerThen: 0.6,
+  //   grams_co2: 0.3,
+  // };
 
   const respone = await fetch(`${endpoint}/${id}`, {
     method: "put",
