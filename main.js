6249b3ba7f6414b368138ed81a25f53a623e80d6
Magnus <magn902m@stud.kea.dk>
2022-04-07 15:01:24 +0200

    test data with object one in the database

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "./sass/style.scss";
 import { get, post, deleteIt, put } from "./js/restdb.js";
 import { getCarbonApi, getPageSpeedApi } from "./js/api.js";
-import { build, combineData } from "./js/portraying_data.js";
-import { submitInput } from "./js/collecting_data.js";
+import { build, combineData, httpsURL } from "./js/portraying_data.js";
+// import { submitInput } from "./js/collecting_data.js";
 
 const apikey = "6245613d67937c128d7c9394";
 const endpoint = "https://frontend-54ac.restdb.io/rest/sustainable-web";
@@ -11,22 +11,23 @@ window.addEventListener("DOMContentLoaded", setup);
 function setup() {
   console.log("setup");
   regBtn();
-  // buildList();
   // console.log(buildList);
   // buildList();
 }
 
 async function buildList() {
   const data = await get(endpoint, apikey);
+  const firstObject = data[0];
   document.querySelector(".result_wrapper").innerHTML = "";
-  data.forEach(build);
+  // data.forEach(build);
+  build(firstObject);
 }
 
 async function regBtn() {
   const form = document.querySelector("form");
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const urlInput = form.elements.url.value;
+    const urlInput = httpsURL(form.elements.url.value);
     console.log("urlInput", urlInput);
 
     async function useJSONApi(urlInput) {
@@ -36,6 +37,7 @@ async function regBtn() {
       const getCarbonData = await getCarbonApi(urlCarbon);
       const getPageSpeedData = await getPageSpeedApi(urlPageSpeed);
       const fullObject = combineData(getCarbonData, getPageSpeedData);
+      console.log(fullObject);
       return fullObject;
     }
 
@@ -45,16 +47,19 @@ async function regBtn() {
       const getCarbonData = await getCarbonApi(urlCarbon);
       const getPageSpeedData = await getPageSpeedApi(urlPageSpeed);
       const fullObject = combineData(getCarbonData, getPageSpeedData);
+      console.log(fullObject);
       return fullObject;
     }
 
+    //* useJSONApi *
     const fullObject = await useJSONApi(urlInput);
-    // const fullObject = await useApi(urlInput);
-
-    console.log(fullObject);
-    // post(endpoint, apikey, fullObject);
-
     buildList();
+
+    // //* useApi *
+    // const fullObject = await useApi(urlInput);
+    // console.log(fullObject, "ready to post");
+    // post(endpoint, apikey, fullObject);
+    // buildList();
 
     // window.location = `results.html?url=${payload.url}&industry=${payload.industry}`;
     // window.location = `print_data.html?url=${payload.url}&industry=${payload.industry}`;
