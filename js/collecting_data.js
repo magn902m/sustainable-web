7abb756e2f127001005794e8c67c9020a576cd26
Magnus <magn902m@stud.kea.dk>
2022-04-07 08:40:00 +0200

    improved submitInput()

--- js/collecting_data.js
+++ js/collecting_data.js
@@ -1,19 +1,35 @@
-export function submitData(obj) {
+export function submitInput(input) {
+  const form = document.querySelector("form");
+  const payload = {
+    url: form.elements.url.value,
+    industry: form.elements.industry.value,
+  };
+
+  // console.log("collecting payload", payload);
+  input = payload;
+  console.log("input", input);
+
+  // window.location = `results.html?url=${payload.url}&industry=${payload.industry}`;
+  // window.location = `print_data.html?url=${payload.url}&industry=${payload.industry}`;
+  return payload;
+}
+
+/*export function submitData(obj) {
   const form = document.querySelector("form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const obj = {
-      company: form.elements.name.value,
-      url: form.elements.price.value,
+      // company: "form.elements.name.value",
+      url: form.elements.url.value,
       industry: form.elements.industry.value,
-      green: form.elements.green.value,
-      cleanerThen: Number(form.elements.cleanerThen),
-      grams_co2: Number(form.elements.grams_co2.value),
+      // green: "form.elements.green.value",
+      // cleanerThen: "Number(form.elements.cleanerThen)",
+      // grams_co2: "Number(form.elements.grams_co2.value)",
     };
 
     console.log(obj);
 
     return obj;
   });
-}
+}*/
